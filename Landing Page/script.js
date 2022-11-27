a23d3c14bdc450fc1e2ca4607489d9174f610490
Beatris Antunes Silva <79115923+beatrisantunes@users.noreply.github.com>
2022-11-26 22:13:07 -0300

    2º Etapa -Landing Page

---
+++ Landing Page/script.js
@@ -0,0 +1,71 @@
+let count = 1;
+document.getElementById("radio__one").checked = true;
+
+setInterval(function() {
+    nextImage();
+}, 2000)
+
+function nextImage() {
+    count++;
+    if (count > 4) {
+        count = 1;
+    }
+    document.getElementById("radio" + count).checked = true;
+
+}
+
+/* Scroll reveal - 
+Mostrar o elemento quando der scroll na página */
+
+const scrollReveal = ScrollReveal({
+    origin: "top",
+    distance: "5px",
+    duration: 1000,
+    reset: true,
+});
+
+scrollReveal.reveal(
+    `
+ .about__me,
+  .about,
+   .contact,
+    .cards,
+      .footer,
+        .formation,
+           .head,
+            .hobbies,
+              .projects,
+                .skills `, { interval: 200 }
+);
+
+const nav = document.querySelector("header .container nav");
+
+const toggle = document.querySelectorAll(".toggle");
+
+for (element of toggle) {
+    element.addEventListener("click", () => {
+        nav.classList.toggle("show");
+    });
+}
+
+const links = document.querySelectorAll(
+    "nav .about .container_cards, nav .skills, nav .menu-social li a"
+);
+
+for (const link of links) {
+    link.addEventListener("click", () => {
+        nav.classList.remove("show");
+    });
+}
+$(document).ready(function() {
+    $("body").append("<a href='#' class='back-to-top'><i class='fa fa-arrow-circle-up' aria-hidden='true'></i></a>"),
+        $(function toTop() {
+            $(window).scroll(function() {
+                $(this).scrollTop() > 200 ? $(".back-to-top").fadeIn() : $(".back-to-top").fadeOut()
+            }), $(".back-to-top").click(function() {
+                return $("html, body").animate({
+                    scrollTop: 0
+                }, 500), !1
+            })
+        });
+})
